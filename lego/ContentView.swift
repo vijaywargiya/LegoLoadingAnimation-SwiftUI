af16feb1a7c1ef5bec2b3f976e9e697ae15bb7a7
Shivam Vijaywargiya <shivamvijaywargiya@gmail.com>
2020-07-17 13:57:36 +0530

    Initial Commit

---
+++ lego/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  lego
+//
+//  Created by Shivam Vijaywargiya on 17/07/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ lego/legoApp.swift
@@ -0,0 +1,17 @@
+//
+//  legoApp.swift
+//  lego
+//
+//  Created by Shivam Vijaywargiya on 17/07/20.
+//
+
+import SwiftUI
+
+@main
+struct legoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
